8d288aebab61b1a9e72fdae70749f376fdd758f2
gettindatfoshow <robert.dale.morris@gmail.com>
2016-10-08 09:47:35 -0400

    ROUTED PAGES!

--- README.md
+++ README.md
@@ -1,16 +1,22 @@
 # team-6
 
-Welcome to the Accelerated Delware's Coffee Shop Web application README.md
+Welcome to the Accelerated Delaware's Coffee Shop Web application README.md
 This project is an application hosted on webserver that facilitates ease of access due to an availability accross multiple 
 devices and operating systems.
 The app will have accesss to mongo database using blaze.js api. The functionality of blaze.js, html, and css is created
-using meteor.js. 
-This app provides its users with an easy way to log in via google, facebook, or twiter handles which keeps users information 
+using meteor.
+This app would provides its users with an easy way to log in via google, facebook, or twiter handles which keeps users information 
 secure.
-The coffee shop web application is design to give its users a information and locations of popular social interactions in Delaware
+The coffee shop web application is design to give its users information and locations of popular social intReeractions in Delaware
 along with Events hosted by partners of Acceleraterd Delaware. Accelerated Delware is a non-profit organizationn aimed at creating
 an enviorment for talented young professionls to Live, Work, Play, and Stay in Delaware.
 The Coffee Shop aims to keep the users activitly enriched and excited using a GAME THEME style appoarch. 
-Users can gain Popularity (XP) by visiting and engaging in social events with other users of the application.
-Users will be able to unlock new avitars, coupons, ect by leveling up from visiting events.
-This should keep users active on the web app, out and about, and meeting new people. 
+Users can gain Popularity (Reputation) by visiting and engaging in social events with other users of the application.
+Users will be able to unlock new avitars, coupons, earn rewards ect by leveling up from visiting events and connecting with others 
+at these events. This should keep users active on the web app, out and about, and meeting new people. Aside for the users engaged 
+in this application, the event hosts or partners events good also gain in popularity by the crowds attracted by those users who activitly 
+checked in while located at the event. 
+Using mongoDB we could keep track of the data or popularity of an event or location based on the users and their connections made. We could use
+this data in attracting new partners and hosts to the platform. We could also randomly ask our users for ideas and input
+on opening new business or setting up events that people wish for.
+
--- coffee_shop/client/main.js
+++ coffee_shop/client/main.js
@@ -3,31 +3,25 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
+//CsEvents = new Mongo.Collection('csevents');
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
-
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
-});
+// if (Meteor.isClient){
+//     Template.body.helpers({
+//        csevents: function(){
+//            return CsEvents.find();
+//        }
+//     });
+// }
 
 if (Meteor.isClient) {
     Meteor.startup(function() {
         GoogleMaps.load();
-    });
+        Session.setDefault("templateName", "main");
+    })
+}
+Template.body.helpers({
+        exampleMap: function() {
 
-    Template.body.helpers({
-        exampleMapOptions: function() {
             // Make sure the maps API has loaded
             if (GoogleMaps.loaded()) {
                 // Map initialization options
@@ -39,21 +33,175 @@ if (Meteor.isClient) {
         }
     });
 
+
     Template.body.onCreated(function() {
         // We can use the `ready` callback to interact with the map API once the map is ready.
-        GoogleMaps.ready('exampleMap', function(map) {
+        GoogleMaps.ready('exampleMap', function (map) {
             // Add a marker to the map once it's ready
             var marker = new google.maps.Marker({
                 position: map.options.center,
                 map: map.instance,
                 title: 'Hello World'
             });
-            var postion2= new google.maps.LatLng(35,783855,-75.549027);
-            var marker2= new google.maps.Marker({
+            var postion2 = new google.maps.LatLng(35, 783855, -75.549027);
+            var marker2 = new google.maps.Marker({
                 position: map.options.position2,
                 map: map.instance,
                 title: 'new marker'
             });
         });
+
     });
-}
+
+    Router.route('/',{
+        name: 'home',
+        template: 'home',
+    });
+
+    Router.route('/register',{
+        name: 'register',
+        template: 'register'
+    });
+    Router.route('/events',{
+        name: 'events',
+        template: 'events'
+    });
+    Router.route('/login', {
+        name: 'login',
+        template: 'login'
+    });
+
+    Router.route('/reputation', {
+        name: 'reputation',
+        template: 'reputation'
+    });
+
+    Router.configure({
+        layoutTemplate: 'main'
+    });
+
+var supportedTypes = ['Map', 'StreetViewPanorama'];
+
+GoogleMaps = {
+    load: _.once(function(options) {
+        options = _.extend({ v: '3.exp' }, options);
+        var params = _.map(options, function(value, key) { return key + '=' + value; }).join('&');
+        var script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.src = 'https://maps.googleapis.com/maps/api/js?' + params +
+            '&callback=GoogleMaps.initialize';
+
+        document.body.appendChild(script);
+    }),
+    utilityLibraries: [],
+    loadUtilityLibrary: function(path) {
+        this.utilityLibraries.push(path);
+    },
+    _loaded: new ReactiveVar(false),
+    loaded: function() {
+        return this._loaded.get();
+    },
+    maps: {},
+    _callbacks: {},
+    initialize: function() {
+        this._loaded.set(true);
+        _.each(this.utilityLibraries, function(path) {
+            var script = document.createElement('script');
+            script.type = 'text/javascript';
+            script.src = path;
+
+            document.body.appendChild(script);
+        });
+    },
+    _ready: function(name, map) {
+        _.each(this._callbacks[name], function(cb) {
+            if (_.isFunction(cb)) {
+                cb(map);
+            }
+        });
+    },
+    ready: function(name, cb) {
+        if (! this._callbacks[name]) {
+            this._callbacks[name] = [];
+        }
+        // make sure we run the callback only once
+        // as the tilesloaded event will also run after initial load
+        this._callbacks[name].push(_.once(cb));
+    },
+    // options: function(options) {
+    //   var self = this;
+    //   return function() {
+    //     if (self.loaded())
+    //       return options();
+    //   };
+    // },
+    get: function(name) {
+        return this.maps[name];
+    },
+    _create: function(name, options) {
+        var self = this;
+        self.maps[name] = {
+            instance: options.instance,
+            options: options.options
+        };
+
+        if (options.type === 'StreetViewPanorama') {
+            options.instance.setVisible(true);
+            self._ready(name, self.maps[name]);
+        } else {
+            google.maps.event.addListener(options.instance, 'tilesloaded', function() {
+                self._ready(name, self.maps[name]);
+            });
+        }
+    },
+    create: function(options) {
+        // default to Map
+        var type = options.type ? options.type : 'Map';
+        if (! _.include(supportedTypes, type)) {
+            throw new Meteor.Error("GoogleMaps - Invalid type argument: " + type);
+        }
+
+        this._create(options.name, {
+            type: type,
+            instance: new google.maps[type](options.element, options.options),
+            options: options.options
+        });
+    }
+};
+
+Template.googleMap.onRendered(function() {
+    var self = this;
+    self.autorun(function(c) {
+        // if the api has loaded
+        if (GoogleMaps.loaded()) {
+            var data = Template.currentData();
+
+            if (! data.options) {
+                return;
+            }
+            if (! data.name) {
+                throw new Meteor.Error("GoogleMaps - Missing argument: name");
+            }
+
+            self._name = data.name;
+
+            var canvas = self.$('.map-canvas').get(0);
+
+            GoogleMaps.create({
+                name: data.name,
+                type: data.type,
+                element: canvas,
+                options: data.options
+            });
+
+            c.stop();
+        }
+    });
+});
+
+Template.googleMap.onDestroyed(function() {
+    if (GoogleMaps.maps[this._name]) {
+        google.maps.event.clearInstanceListeners(GoogleMaps.maps[this._name].instance);
+        delete GoogleMaps.maps[this._name];
+    }
+});
